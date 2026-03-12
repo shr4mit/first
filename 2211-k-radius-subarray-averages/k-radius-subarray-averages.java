d68f529d6f79874c93698ad6d96cc5c6ac0364f3
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-03-12 11:44:11 +0530

    Time: 6 ms (68.35%) | Memory: 175.2 MB (59.92%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int arr[]=new int [nums.length];
+        Arrays.fill(arr,-1);
+        long win=0;
+        int idx=2*k+1;
+        int kk=k;
+        if(idx>arr.length){
+            return arr;
+        }
+        for(int i=0;i<idx;i++){
+            win+=nums[i];
+        }
+      
+        arr[kk]=(int)(win/idx);
+        for(int i=idx;i<arr.length;i++){
+            win=win-nums[i-idx]+nums[i];
+            arr[++kk]=(int)(win/idx);
+                    }
+                    return arr;
+    }
+}
