6078bf91b2cfd48c010966020b81cc972ab5e57f
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-08-12 21:25:53 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.13%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,13 +1,9 @@
-class Solution {
+class Solution{
     public boolean checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0){
-                sum+=i;
-            }
-        }
-        if(sum==num){
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
             return true;
-        }else return false;
+        } else {
+            return false;
+        }
     }
 }
