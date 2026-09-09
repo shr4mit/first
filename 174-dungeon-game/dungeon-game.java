6d86a31d1f81c510577da070d94056339328b89f
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-09 11:56:04 +0530

    Time: 2 ms (68.79%) | Memory: 44.8 MB (96.25%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,21 +1,20 @@
 class Solution {
-    int n,m;
-    int dp[][];
-public int fxn(int i, int j, int dungeon[][]){
-    if(i>=n || j>=m) return Integer.MAX_VALUE;
-    if(i==n-1 && j==m-1)
-    return Math.max(1,1-dungeon[i][j]);
-    if(dp[i][j]!=0) return dp[i][j];
-    int r=fxn(i,j+1,dungeon);
-    int d=fxn(i+1,j,dungeon);
-    int res=Math.max(1,Math.min(r,d)-dungeon[i][j]);
-    return dp[i][j]=res;
-}
     public int calculateMinimumHP(int[][] dungeon) {
-        n= dungeon.length;
-        m=dungeon[0].length;
-        dp=new int[n][m];
-        return fxn(0,0,dungeon);      
+        int n=dungeon.length;
+        int m=dungeon[0].length;
+        int[][] dp=new int[n][m];
+        dp[n-1][m-1]=Math.max(1,1-dungeon[n-1][m-1]);
+        for(int j=m-2;j>=0;j--){
+         dp[n-1][j]=Math.max(1,dp[n-1][j+1]-dungeon[n-1][j]);
+        }
+        for(int i=n-2;i>=0;i--){
+        dp[i][m-1]=Math.max(1,dp[i+1][m-1]-dungeon[i][m-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+              dp[i][j]=Math.max(1,Math.min(dp[i][j+1],dp[i+1][j])-dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
     }
-
 }
