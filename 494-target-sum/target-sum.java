f17215bf907f6ba8b14af17ca9c2441add0da8f8
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-16 00:34:00 +0530

    Time: 8 ms (59.60%) | Memory: 46.8 MB (23.11%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,25 @@
 class Solution {
+    static int dp[][];
+    static int offset;
     static int fxn(int arr[], int i,int sum,int target){
         if(i==arr.length){
           if(sum==target) return 1;
         return 0;
         }
+         if(dp[i][sum+offset]!=-1) return dp[i][sum+offset];
         int ex=fxn(arr,i+1,sum-arr[i],target);
         int in=fxn(arr,i+1,sum+arr[i],target);
-        return ex+in;
+        return dp[i][sum+offset]= ex+in;
     }
     public int findTargetSumWays(int[] nums, int target) {
+        int total=0;
+        for(int x:nums) total+=x;
+        offset=total;
+        dp=new int[nums.length+1][2*total+1];
+        for(int x[]:dp){
+            Arrays.fill(x,-1);
+        }
+        if(Math.abs(target)>total) return 0;
         return fxn(nums,0,0,target);
     }
 }
