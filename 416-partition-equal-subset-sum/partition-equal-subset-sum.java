9ece565c6ebedb36a26ea2553935b0454956d085
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-16 00:06:30 +0530

    Time: 48 ms (55.65%) | Memory: 47.6 MB (51.81%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,22 +1,26 @@
 class Solution {
-    static Boolean dp[][];
-    static boolean fxn(int arr[],int i,int total,int sum){
-     if(i==arr.length) return false;
-     if(sum==total/2) return true;
-     if(dp[i][sum]!=null) return dp[i][sum];
-     boolean ex=fxn(arr,i+1,total,sum);
-     boolean in=fxn(arr,i+1,total,sum+arr[i]);
-   return dp[i][sum]=ex||in;
-    }
     public boolean canPartition(int[] nums) {
         int total=0;
-    
-       for(int x: nums){
-         total+=x;
-       } 
-       dp=new Boolean[nums.length+1][total+1];
-       
-       if(total%2!=0) return false;
-       return fxn(nums,0,total,0);
+        for(int x:nums){
+            total+=x;
+        }
+        if(total%2!=0)return false;
+        int target=total/2;
+        boolean dp[][]=new boolean[nums.length+1][target+1];
+        for(int i=0;i<=nums.length;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=nums.length;i++){
+        for(int j=1;j<=target;j++){
+            if(nums[i-1]<=j){
+              dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
+                }
+          else{
+         dp[i][j]=dp[i-1][j];
+            }
+            }
+        }
+
+        return dp[nums.length][target];
     }
 }
