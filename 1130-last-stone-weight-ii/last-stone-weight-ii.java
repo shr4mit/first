86a56b2f9396e94d5f9bd1d53d76f1fb07080df3
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-16 16:05:30 +0530

    Time: 2 ms (91.04%) | Memory: 44.1 MB (21.66%) - LeetSync

---
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int dp[][];
+    static int fxn(int arr[],int i,int sum1,int sum2, int total ){
+        if(i==arr.length) return Math.abs(sum1-sum2);
+        if(dp[i][sum1]!=-1) return dp[i][sum1];
+        int in=fxn(arr,i+1,sum1+arr[i],sum2,total);
+        int ex=fxn(arr,i+1,sum1,sum2+arr[i],total);
+        return dp[i][sum1]=Math.min(ex,in);
+    }
+    public int lastStoneWeightII(int[] stones) {
+    int total=0;
+    for(int x:stones) total+=x;
+        dp=new int[stones.length][total+1];
+        for(int x[]:dp){
+            Arrays.fill(x,-1);
+        }
+        return fxn(stones,0,0,0,total);
+    }
+}
