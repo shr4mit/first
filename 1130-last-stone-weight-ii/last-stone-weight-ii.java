a31002ecd255b8763c6b2996b07006841a3171f5
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-17 16:21:48 +0530

    Time: 6 ms (17.50%) | Memory: 44.4 MB (16.49%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -1,19 +1,28 @@
-class Solution {
-    static int dp[][];
-    static int fxn(int arr[],int i,int sum1,int sum2 ){
-        if(i==arr.length) return Math.abs(sum1-sum2);
-        if(dp[i][sum1]!=-1) return dp[i][sum1];
-        int in=fxn(arr,i+1,sum1+arr[i],sum2);
-        int ex=fxn(arr,i+1,sum1,sum2+arr[i]);
-        return dp[i][sum1]=Math.min(ex,in);
-    }
-    public int lastStoneWeightII(int[] stones) {
-    int total=0;
-    for(int x:stones) total+=x;
-        dp=new int[stones.length][total+1];
-        for(int x[]:dp){
-            Arrays.fill(x,-1);
+class Solution{
+    public int lastStoneWeightII(int[] stones){
+        int total=0;
+        for(int x:stones) total+=x;
+        int dp[][]=new int[stones.length+1][total+1];
+        dp[0][0]=1;
+        for(int i=1;i<=stones.length;i++){
+            dp[i][0]=1;
+            for(int j=1;j<=total;j++){
+                int ex=dp[i-1][j];
+                int in=0;
+                if(j>=stones[i-1]){
+                    in=dp[i-1][j-stones[i-1]];
+                }
+                dp[i][j]=Math.max(ex,in);
+            }
         }
-        return fxn(stones,0,0,0);
+        int min=total;
+        for(int j=0;j<=total/2;j++){
+            if(dp[stones.length][j]==1){
+                int s1=j;
+                int s2=total-j;
+                min=Math.min(min,s2-s1);
+            }
+        }
+        return min;
     }
 }
