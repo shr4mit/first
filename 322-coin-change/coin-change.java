cf0ad960300f05a1e7efee5b158eae80cc70cf77
Shramit <138600564+shr4mit@users.noreply.github.com>
2026-09-20 21:17:37 +0530

    Time: 21 ms (36.81%) | Memory: 46.3 MB (73.42%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,15 @@
 class Solution {
-    static int dp[][];
-    static int fxn(int arr[], int i, int target,int sum){
-        if(sum==target){ return 0;
-        }
-         if(i==arr.length||sum>target)
-         return 10000000;
-        
-         if(dp[i][sum]!=-1) return dp[i][sum];
-
-        int ex=fxn(arr,i+1,target,sum);
-        int in =100000000;
-         if(arr[i]<=target-sum)
-         in =1+fxn(arr,i,target,sum+arr[i]);
-        return dp[i][sum]= Math.min(in,ex);
-    }
     public int coinChange(int[] coins, int amount) {
-        dp=new int[coins.length+1][amount+1];
-        for(int x[]:dp){
-            Arrays.fill(x,-1);
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int c:coins){
+                if(i>=c){
+                    dp[i]=Math.min(dp[i],dp[i-c]+1);
+                }
+            }
         }
-        int ans= fxn(coins,0,amount,0);
-        if(ans>=10000000){
-            return -1;
-        }
-        return ans;
+        return dp[amount]>amount?-1:dp[amount];
     }
 }
